032bf8a91d304eb8e11eb89faf143aeb7a16c4e1
Vladas Zakrevksis <146100@gmail.com>
2021-06-23 03:02:41 +0300

    make box and platforms

--- src/platform.rs
+++ src/platform.rs
@@ -4,4 +4,7 @@ pub struct Platform;
 impl Platform {
     pub const MAC: bool = cfg!(target_os = "macos");
     pub const WIN: bool = cfg!(target_os = "windows");
+    pub const IOS: bool = cfg!(target_os = "ios");
+    pub const ANDROID: bool = cfg!(target_os = "android");
+    pub const MOBILE: bool = Platform::IOS || Platform::ANDROID;
 }
--- src/refs.rs
+++ src/refs.rs
@@ -8,3 +8,7 @@ pub type DynWeak<T> = Option<MutWeak<T>>;
 pub fn make_shared<T>(val: T) -> Shared<T> {
     Rc::new(RefCell::new(val))
 }
+
+pub fn make_box<T>(val: T) -> Box<T> {
+    Box::new(val)
+}
