21a574acacfade29edea4eb73b71858fb334781c
Vladas Zakrevskis <146100@gmail.com>
2021-11-23 19:17:29 +0200

    signal example

---
+++ examples/signals.rs
@@ -0,0 +1,21 @@
+use tokio::task;
+use tokio::time::{sleep, Duration};
+
+#[tokio::main]
+async fn main() -> Result<(), Error> {
+    let join = task::spawn(async {
+        let mut result = 0;
+        for i in 0..100 {
+            dbg!(i);
+            dbg!(&result);
+            result += i;
+            sleep(Duration::from_millis(100)).await;
+        }
+        result
+    });
+
+
+    let result = join.await?;
+
+    Ok(())
+}
