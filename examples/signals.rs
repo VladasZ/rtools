9e8ed9bc00e548d5143d446210cfad10c522ad0a
Vladas Zakrevskis <146100@gmail.com>
2021-11-24 13:28:43 +0200

    signal hook test

--- examples/signals.rs
+++ examples/signals.rs
@@ -1,24 +1,78 @@
-// use tokio::{
-//     task,
-//     time::{sleep, Duration},
-// };
+use tokio::{
+    task,
+    time::{sleep, Duration},
+};
 
-// #[tokio::main]
-// async fn main() -> Result<(), Error> {
-//     let join = task::spawn(async {
-//         let mut result = 0;
-//         for i in 0..100 {
-//             dbg!(i);
-//             dbg!(&result);
-//             result += i;
-//             sleep(Duration::from_millis(100)).await;
-//         }
-//         result
-//     });
+use std::io::Error;
 
-//     let result = join.await?;
+use signal_hook::consts::signal::*;
+use signal_hook_tokio::Signals;
 
-//     Ok(())
-// }
+use futures::stream::StreamExt;
 
-fn main() {}
+
+async fn handle_signals(signals: Signals) {
+    let mut signals = signals.fuse();
+    while let Some(signal) = signals.next().await {
+
+        match signal {
+            SIGHUP => {
+                dbg!("SIGHUP");
+            }
+            SIGTERM => {
+                dbg!("SIGTERM");
+            },
+            SIGINT => {
+                dbg!("SIGINT");
+            },
+            SIGQUIT => {
+                dbg!("SIGQUIT");
+            },
+            _ => unreachable!(),
+        }
+    }
+}
+
+
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>>  {
+
+    let signals = Signals::new(&[
+        SIGHUP,
+        SIGTERM,
+        SIGINT,
+        SIGQUIT,
+    ])?;
+
+    let handle = signals.handle();
+    let signals_task = tokio::spawn(handle_signals(signals));
+
+
+    let join = task::spawn(async {
+        let mut result = 0;
+        for i in 0..10 {
+            dbg!(i);
+            dbg!(&result);
+            result += i;
+            sleep(Duration::from_secs(1)).await;
+        }
+        result
+    });
+
+    let result = join.await?;
+
+    dbg!(result);
+
+    // Terminate the signal stream.
+
+    dbg!("handle");
+
+    handle.close();
+    dbg!("signals_task");
+    signals_task.await?;
+
+    dbg!("bye");
+
+    Ok(())
+}
