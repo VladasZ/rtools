b442643bd35caab032f5dacadd9aff6e4b1d7a51
Vladas Zakrevskis <146100@gmail.com>
2021-11-25 18:30:49 +0200

    win signals

--- examples/signals.rs
+++ examples/signals.rs
@@ -1,7 +1,7 @@
 use std::{
     future::Future,
     sync::{
-        atomic::{AtomicBool, Ordering},
+        atomic::{AtomicBool, AtomicU64, Ordering},
         Arc,
     },
 };
@@ -15,13 +15,13 @@ use tokio::{
 };
 
 struct SignalsHandler {
-    cancelled: AtomicBool,
+    cancelled: AtomicU64,
 }
 
 impl SignalsHandler {
     fn new() -> Arc<Self> {
         let new = Arc::new(Self {
-            cancelled: Default::default(),
+            cancelled: 0.into(),
         });
 
         let moved = new.clone();
@@ -36,7 +36,7 @@ impl SignalsHandler {
 
                 dbg!("OOO COTRO COO!!");
 
-                moved.cancelled.store(true, Ordering::Relaxed);
+                moved.cancelled.store(cancelled + 1, Ordering::Relaxed);
             }
         });
 
@@ -44,16 +44,21 @@ impl SignalsHandler {
     }
 }
 
-#[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error>> {
+const signals: &[wintrap::Signal] = &[
+    wintrap::Signal::CtrlC,
+    wintrap::Signal::CloseWindow,
+    wintrap::Signal::CloseConsole,
+    wintrap::Signal::CtrlBreak,
+];
+
+async fn slot() -> Result<(), Box<dyn std::error::Error>> {
     let handler = SignalsHandler::new();
 
-    let join = task::spawn(async {
+    let join = task::spawn(async move {
         let mut result = 0;
-        for i in 0..20 {
-            dbg!(i);
-            dbg!(&result);
+        for i in 0..200000 {
             result += i;
+            dbg!(handler.cancelled.load(Ordering::Relaxed));
             sleep(Duration::from_secs(1)).await;
         }
         result
@@ -62,6 +67,35 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let result = join.await?;
 
     dbg!(result);
+
+    Ok(())
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+
+    let forg = wintrap::trap(
+        &signals,
+        |signal| {
+            // handle signal here
+            println!("Caught a signal: {:?}", signal);
+            unsafe { windows::Win32::System::Diagnostics::Debug::Beep(2000, 50) };
+        },
+        || {
+            println!("Doing work");
+            // do work
+            for i in 0..200000 {
+                //  result += i;
+                // dbg!(handler.cancelled.load(Ordering::Relaxed));
+                std::thread::sleep(Duration::from_secs(1));
+                //sleep(Duration::from_secs(1)).await;
+            }
+            println!("Doing work");
+        },
+    )
+        .unwrap();
+
+    slot().await;
 
     Ok(())
 }
