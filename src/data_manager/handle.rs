bc64ce5a69da67499d93b02ffc314a005562e2c6
Vladas Zakrevskis <146100@gmail.com>
2022-04-10 23:20:54 +0300

    managed handle

--- src/data_manager/handle.rs
+++ src/data_manager/handle.rs
@@ -1,14 +1,63 @@
-use std::marker::PhantomData;
+use std::{marker::PhantomData, ops::Deref};
 
 use crate::data_manager::Managed;
 
-pub struct Handle<T> {
-    _hash: u64,
+#[derive(Debug)]
+pub struct Handle<T: Managed> {
+    hash:  u64,
     _data: PhantomData<T>,
 }
 
 impl<T: Managed> Handle<T> {
-    fn _get(&self) -> &T {
-        todo!()
+    pub fn is_ok(&self) -> bool {
+        !self.is_null()
+    }
+
+    pub fn is_null(&self) -> bool {
+        self.hash == u64::MAX
+    }
+
+    pub fn get(&self) -> Option<&T> {
+        if self.is_null() {
+            return None;
+        }
+        T::get_by_hash(self.hash).into()
+    }
+}
+
+impl<T: Managed> Deref for Handle<T> {
+    type Target = T;
+    fn deref(&self) -> &T {
+        debug_assert!(self.is_ok());
+        T::get_by_hash(self.hash)
+    }
+}
+
+impl<T: Managed> Default for Handle<T> {
+    fn default() -> Self {
+        Self {
+            hash:  u64::MAX,
+            _data: Default::default(),
+        }
+    }
+}
+
+impl<T: Managed> From<u64> for Handle<T> {
+    fn from(hash: u64) -> Self {
+        Self {
+            hash,
+            _data: Default::default(),
+        }
+    }
+}
+
+impl<T: Managed> Copy for Handle<T> {}
+
+impl<T: Managed> Clone for Handle<T> {
+    fn clone(&self) -> Self {
+        Self {
+            hash:  self.hash,
+            _data: Default::default(),
+        }
     }
 }
--- src/data_manager/mod.rs
+++ src/data_manager/mod.rs
@@ -1,9 +1,6 @@
 mod handle;
 
-use std::{
-    collections::HashMap,
-    path::{Path, PathBuf},
-};
+use std::{collections::HashMap, path::Path};
 
 pub use handle::Handle;
 
@@ -13,19 +10,35 @@ pub type DataStorage<T> = HashMap<u64, T>;
 
 pub trait Managed: 'static + LoadFromPath + DataManager<Self> {}
 
-pub trait LoadFromPath: Sized + Clone {
+pub trait LoadFromPath: Sized {
     fn load(path: &Path) -> Self;
 }
 
-pub trait DataManager<T: 'static + LoadFromPath> {
-    fn path() -> PathBuf;
+pub trait DataManager<T: 'static + Managed> {
+    fn path() -> &'static Path;
     fn set_path(path: &Path);
 
     fn storage() -> &'static mut DataStorage<T>;
 
-    fn get(name: &str) -> &T {
+    fn add_with_hash(hash: u64, resource: T) -> Handle<T> {
+        let storage = Self::storage();
+        debug_assert!(
+            !storage.contains_key(&hash),
+            "Object with such hash already exists"
+        );
+        storage.insert(hash, resource);
+        hash.into()
+    }
+
+    fn get_by_hash(hash: u64) -> &'static T {
+        &Self::storage()[&hash]
+    }
+
+    fn get(name: &str) -> Handle<T> {
+        let hash = hash(name);
         Self::storage()
-            .entry(hash(name))
-            .or_insert_with(|| T::load(&Self::path().join(name)))
+            .entry(hash)
+            .or_insert_with(|| T::load(&Self::path().join(name)));
+        hash.into()
     }
 }
