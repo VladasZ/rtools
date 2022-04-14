8e34ab2c2f8ae40c1b8f70a6406df527c7c8f205
Vladas Zakrevskis <146100@gmail.com>
2022-04-15 02:22:56 +0300

    print_thread_name

--- src/misc.rs
+++ src/misc.rs
@@ -1,6 +1,7 @@
 use std::{
     collections::hash_map::DefaultHasher,
     hash::{Hash, Hasher},
+    thread,
 };
 
 use backtrace::Backtrace;
@@ -15,4 +16,12 @@ pub fn hash(obj: impl Hash) -> u64 {
     let mut hasher = DefaultHasher::new();
     obj.hash(&mut hasher);
     hasher.finish()
+}
+
+pub fn print_thread_name() {
+    let thread = thread::current();
+    let thread_name = thread::current().name().unwrap_or_default().to_string();
+    dbg!(&thread_name);
+    dbg!(thread.id());
+    error!("thread name: {:?}, id: {:?}", thread_name, thread.id());
 }
