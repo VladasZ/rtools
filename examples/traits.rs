c631bbcd372274548c86bb912fb86720a0b9d2dc
Vladas Zakrevskis <146100@gmail.com>
2022-05-02 00:43:03 +0300

    default trait test

--- examples/traits.rs
+++ examples/traits.rs
@@ -1,7 +1,36 @@
-trait Spilkog {}
+#![allow(incomplete_features)]
+#![feature(specialization)]
+#![feature(trait_upcasting)]
 
-trait SpilkogCallbacks {}
+trait Spilkog: SpilkogCallbacks {}
+
+trait SpilkogCallbacks {
+    fn setup(&self);
+}
+
+impl<T: ?Sized + Spilkog> SpilkogCallbacks for T {
+    default fn setup(&self) {
+        dbg!("Defakto");
+    }
+}
+
+#[derive(Default)]
+struct Strekol;
+impl Spilkog for Strekol {}
+impl SpilkogCallbacks for Strekol {
+    fn setup(&self) {
+        dbg!("implomentalnostk!!");
+    }
+}
+
+#[derive(Default)]
+struct Izok;
+impl Spilkog for Izok {}
 
 fn main() {
-    dbg!("speserglica");
+    let sigo: &dyn Spilkog = &Strekol::default();
+    let izo: &dyn Spilkog = &Izok::default();
+
+    sigo.setup();
+    izo.setup();
 }
