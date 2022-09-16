bb952f1170269f5dba8fe2f4d4b8687632fa4c27
Vladas Zakrevskis <146100@gmail.com>
2022-09-16 19:47:52 +0300

    random_in

--- src/random.rs
+++ src/random.rs
@@ -1,10 +1,13 @@
+use std::ops::Range;
+
 use rand::{
     distributions::{Alphanumeric, DistString},
     thread_rng, Rng,
 };
 
-pub trait Random {
+pub trait Random: Sized {
     fn random() -> Self;
+    fn random_in(range: Range<Self>) -> Self;
 }
 
 impl Random for String {
@@ -12,11 +15,29 @@ impl Random for String {
         let mut rng = thread_rng();
         Alphanumeric.sample_string(&mut rng, 8)
     }
+    fn random_in(_: Range<Self>) -> Self {
+        unimplemented!()
+    }
 }
 
 impl Random for u32 {
     fn random() -> Self {
         let mut rng = thread_rng();
         rng.gen_range(0..100)
+    }
+
+    fn random_in(range: Range<Self>) -> Self {
+        thread_rng().gen_range(range)
+    }
+}
+
+impl Random for usize {
+    fn random() -> Self {
+        let mut rng = thread_rng();
+        rng.gen_range(0..100)
+    }
+
+    fn random_in(range: Range<Self>) -> Self {
+        thread_rng().gen_range(range)
     }
 }
