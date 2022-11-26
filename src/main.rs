604ba07a256b77bdda557c97b76b452168f6fdaa
Vladas Zakrevskis <146100@gmail.com>
2022-11-26 13:34:59 +0200

    more random types

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,14 @@
 #![allow(clippy::mismatched_target_os)]
 
-use rtools::passed::Passed;
+use rtools::{passed::Passed, Random};
 
 fn main() {
     let mut pass = Passed::default();
 
     pass.print();
     pass.print();
+
+    for _ in 0..100 {
+        dbg!(bool::random());
+    }
 }
--- src/random.rs
+++ src/random.rs
@@ -10,6 +10,17 @@ pub trait Random: Sized {
     fn random_in(range: Range<Self>) -> Self;
 }
 
+impl Random for bool {
+    fn random() -> Self {
+        let mut rng = thread_rng();
+        rng.gen_range(0..=1) != 0
+    }
+
+    fn random_in(_: Range<Self>) -> Self {
+        unimplemented!()
+    }
+}
+
 impl Random for String {
     fn random() -> Self {
         let mut rng = thread_rng();
@@ -31,7 +42,40 @@ impl Random for i32 {
     }
 }
 
+impl Random for u64 {
+    fn random() -> Self {
+        let mut rng = thread_rng();
+        rng.gen_range(0..100)
+    }
+
+    fn random_in(range: Range<Self>) -> Self {
+        thread_rng().gen_range(range)
+    }
+}
+
+impl Random for i64 {
+    fn random() -> Self {
+        let mut rng = thread_rng();
+        rng.gen_range(-100..100)
+    }
+
+    fn random_in(range: Range<Self>) -> Self {
+        thread_rng().gen_range(range)
+    }
+}
+
 impl Random for u32 {
+    fn random() -> Self {
+        let mut rng = thread_rng();
+        rng.gen_range(0..100)
+    }
+
+    fn random_in(range: Range<Self>) -> Self {
+        thread_rng().gen_range(range)
+    }
+}
+
+impl Random for isize {
     fn random() -> Self {
         let mut rng = thread_rng();
         rng.gen_range(0..100)
