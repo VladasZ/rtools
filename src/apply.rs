d4d2bcbddb78385bed69ae3b55f2225acc12ada2
Vladas <146100@gmail.com>
2022-11-30 02:54:05 +0200

    tweaking apply

--- src/apply.rs
+++ src/apply.rs
@@ -1,26 +1,22 @@
 pub trait Apply<T> {
-    fn apply(self, action: impl FnMut(&mut T));
-    fn apply2<U, Second: IntoIterator<Item = U>>(
-        self,
-        second: Second,
-        action: impl FnMut(&mut T, U),
-    );
+    fn apply(self, action: impl FnMut(T));
+    fn apply2<U, Second: IntoIterator<Item = U>>(self, second: Second, action: impl FnMut(T, U));
 }
 
 impl<T, I: IntoIterator<Item = T>> Apply<T> for I {
-    fn apply(self, mut action: impl FnMut(&mut T)) {
-        for mut item in self.into_iter() {
-            action(&mut item)
+    fn apply(self, mut action: impl FnMut(T)) {
+        for item in self.into_iter() {
+            action(item)
         }
     }
 
     fn apply2<U, Second: IntoIterator<Item = U>>(
         self,
         second: Second,
-        mut action: impl FnMut(&mut T, U),
+        mut action: impl FnMut(T, U),
     ) {
-        for (mut item, second) in self.into_iter().zip(second.into_iter()) {
-            action(&mut item, second);
+        for (item, second) in self.into_iter().zip(second.into_iter()) {
+            action(item, second);
         }
     }
 }
@@ -33,7 +29,7 @@ mod test {
     fn apply_arr() {
         let mut ve = vec![];
         [1, 2, 3, 4, 5].apply(|a| {
-            ve.push(*a);
+            ve.push(a);
         });
         assert_eq!(&ve, &[1, 2, 3, 4, 5]);
     }
@@ -43,7 +39,7 @@ mod test {
         let mut num = vec![];
         let mut ch = vec![];
         [(1, '5'), (2, '4'), (3, '3'), (4, '2'), (5, '1')].apply(|(n, c)| {
-            num.push(*n);
+            num.push(n);
             ch.push(c.clone());
         });
         assert_eq!(&num, &[1, 2, 3, 4, 5]);
@@ -55,7 +51,7 @@ mod test {
         let mut num = vec![];
         let mut ch = vec![];
         [1, 2, 3, 4, 5].apply2(['5', '4', '3', '2', '1'], |n, c| {
-            num.push(*n);
+            num.push(n);
             ch.push(c);
         });
         assert_eq!(&num, &[1, 2, 3, 4, 5]);
